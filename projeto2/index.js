e9ae825294386c674e6074e9a24a700d53b40c42
Charles Marques <charles_sjp@yahoo.com.br>
2022-03-20 10:22:34 -0300

    +clear

--- projeto2/index.js
+++ projeto2/index.js
@@ -1,30 +1,19 @@
 const prompt = require('prompt-sync')()
-
 console.log("          <<<<JOKENPÔ>>>>       \n\n\n");
 const listaDeOpcoes = ["Pedra","Papel","Tesoura"];
 let vitoriaM = 0;
 let vitoriaS = 0;
 numeroDerodas = +prompt(`Escolha a quantidade de rodadas: `)
-
-//RODADAS DO JOGO
-
 for (let rodadas = 0; rodadas < numeroDerodas; rodadas++) {
-    
 console.log(`\n-Digite [0] para ${listaDeOpcoes[0]}\n-Digite [1] para ${listaDeOpcoes[1]}\n-Digite [2] para ${listaDeOpcoes[2]}\n`)
 escolhaSua = +prompt("Digite uma das opções acima: ")
-
-//ESCOLHAS DAS OPÇÕES
-
+console.clear()
 while (escolhaSua!=0 &&escolhaSua!=1 && escolhaSua!=2) {
-    
     console.log("Opção inválida\n")
     console.log(`-Digite [0] para ${listaDeOpcoes[0]}\n-Digite [1] para ${listaDeOpcoes[1]}\n-Digite [2] para ${listaDeOpcoes[2]}\n`)
     escolhaSua = +prompt("Digite uma das opções acima: ")
 }
-
-//ESCOLHA ALEATÓRIA DO COMPUTADOR
 let escolhaMaquina = Math.floor(Math.random()*3)
-
 if (escolhaSua == 0) {
     console.log(`\nVocê escolheu ${listaDeOpcoes[0]}\n`);
 }
@@ -33,7 +22,6 @@ if (escolhaSua == 1) {
 }
 if (escolhaSua == 2) {
     console.log(`Você escolheu ${listaDeOpcoes[2]}\n`);
-
 }
 if (escolhaMaquina == 0){
     console.log(`O computador escolheu ${listaDeOpcoes[0]}\n`);
@@ -44,9 +32,6 @@ if (escolhaMaquina == 1){
 if (escolhaMaquina == 2){
     console.log(`O computador escolheu ${listaDeOpcoes[2]}\n`);
 }    
-
-//escolha sua x escolha da máquina
-
 if (escolhaSua==escolhaMaquina) {
     console.log(`EMPATE!\n`);
 }
@@ -62,7 +47,6 @@ if (escolhaSua==2&&escolhaMaquina==1) {
     console.log(`[${listaDeOpcoes[2]} x ${listaDeOpcoes[1]}]\n Boa, você GANHOU!\n `);
     vitoriaS++
 }    
-    
 if (escolhaSua==2&&escolhaMaquina==0) {
     console.log(`[${listaDeOpcoes[2]} x ${listaDeOpcoes[0]}]\n Que pena, você PERDEU!\n `);
     vitoriaM++
@@ -77,16 +61,11 @@ if (escolhaSua==1&&escolhaMaquina==2) {
 }
 console.log(`Escore parcial MÁQUINA ${vitoriaM} x ${vitoriaS} JOGADOR\n`);    
 }
-
-// ESCORE FINAL
 console.log("          <<<<FIM DO JOGO>>>>       \n\n\n");
-
 if (vitoriaS>vitoriaM){
-    console.log("VOCẼ É O CAMPEÃO");
-    
+    console.log("VOCẼ É O CAMPEÃO");   
 }else if (vitoriaS==vitoriaM) {
     console.log("O jogo terminou EMPATADO");
 }else{
     console.log("VOCÊ PERDEU O JOGO");
 }
-
